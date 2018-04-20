d254cb10f5fd1ce2c586ca12a75f608b59ff4e29
MaHaoZhe <411408809@qq.com>
2018-04-20 10:06:45 +0800

    Initial Commit

---
+++ HZCommonUtil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HZCommonUtil
+//
+//  Created by MaHaoZhe on 2018/4/20.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
